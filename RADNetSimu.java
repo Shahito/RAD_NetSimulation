d87b07c694f9fe45d3c115b24c7729f136173551
Shahito <76411763+Shahito@users.noreply.github.com>
2021-01-29 17:24:07 +0100

    Update RADNetSimu.java

--- RADNetSimu.java
+++ RADNetSimu.java
@@ -8,28 +8,5 @@ import java.util.Arrays;
 public class RADNetSimu {
 	public static void main(String[] args) throws IOException {
 		
-		/*AddrIp ipaddr1 = new AddrIp();
-		AddrMac macaddr1 = new AddrMac();
-		
-		short[] ip1 = {0,0,0,0};
-		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-		for(int n=0;n<4;n++) {
-			short ipelem=Short.parseShort(reader.readLine());
-			ip1[n]=ipelem;
-		}
-		
-		short[] mask1={0,0,0,0};
-		for(int n=0;n<4;n++) {
-			short maskelem=Short.parseShort(reader.readLine());
-			mask1[n]=maskelem;
-		}
-		
-		ipaddr1.setIp(ip1);
-		ipaddr1.setMask(mask1);
-		System.out.println(Arrays.toString(ipaddr1.getIp()));
-		System.out.println(Arrays.toString(ipaddr1.getMask()));
-		String[] mac1={"ef","ae","d7","fd","29","38"};
-		macaddr1.setMac(mac1);
-		System.out.println(Arrays.toString(macaddr1.getMac()));*/
 	}
 }
